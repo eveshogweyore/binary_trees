96e1ff2605a7f286a151eb5b9005d6e02c966d4a
Gift <aghaulor.gift@gmail.com>
2024-03-25 15:46:26 -0700

    Create new node & header file.

---
+++ 0-binary_tree_node.c
@@ -0,0 +1,21 @@
+#include "binary_trees.h"
+
+/**
+ * binary_tree_node- The binary tree node to be created.
+ * @parent: The pointer to the parent node.
+ * @value: The value of the node.
+ *
+ * Return: A pointer to the new node, or NULL on failure
+ */
+
+binary_tree_t *binary_tree_node(binary_tree_t *parent, int value)
+{
+	binary_tree_t *newNode = malloc(sizeof(binary_tree_t));
+
+	newNode->n = value;
+	newNode->left = NULL;
+	newNode->right = NULL;
+	newNode->parent = parent;
+
+	return (newNode);
+}
