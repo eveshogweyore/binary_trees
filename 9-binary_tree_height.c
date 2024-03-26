80e622424cdbb9832143bb5d43147764ce93bcd7
Eveshogweyore Alle <alleeveshogweyore@gmail.com>
2024-03-26 21:19:16 +0100

    Bug fix: Adjust max_value() function's parameters and return value set to integer.

--- 9-binary_tree_height.c
+++ 9-binary_tree_height.c
@@ -1,7 +1,14 @@
 #include "binary_trees.h"
 
+/**
+ * max_value - Compares two numbers and returns the maximum value.
+ * @fnum: The first number.
+ * @snum: The second number.
+ *
+ * Return: On success, the maximum number.
+ */
 
-size_t max_value(size_t fnum, size_t snum)
+int max_value(int fnum, int snum)
 {
 	if (fnum > snum)
 		return (fnum);
@@ -9,18 +16,20 @@ size_t max_value(size_t fnum, size_t snum)
 	return (snum);
 }
 
+
 /**
  * binary_tree_height - Measure the height of a binary tree.
  * @tree: Pointer the root node of binary tree.
  *
  * Return: On success, height of tree. On failure, 0.
  */
+
 size_t binary_tree_height(const binary_tree_t *tree)
 {
-	size_t leftHeight, rightHeight; 
+	int leftHeight, rightHeight;
 
 	if (tree == NULL)
-		return (0);
+		return (-1);
 
 	leftHeight = binary_tree_height(tree->left);
 	rightHeight = binary_tree_height(tree->right);
