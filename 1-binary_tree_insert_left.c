efab505510e80818dc8b2fa750fa9c242184288f
Gift <aghaulor.gift@gmail.com>
2024-03-26 07:10:22 -0700

    Fix memory condition

--- 1-binary_tree_insert_left.c
+++ 1-binary_tree_insert_left.c
@@ -11,10 +11,12 @@
 
 binary_tree_t *binary_tree_insert_left(binary_tree_t *parent, int value)
 {
-	binary_tree_t *leftNode = malloc(sizeof(binary_tree_t));
+	binary_tree_t *leftNode;
 
 	if (parent == NULL)
 		return (NULL);
+
+	leftNode = malloc(sizeof(binary_tree_t));
 
 	if (leftNode == NULL)
 		return (NULL);
