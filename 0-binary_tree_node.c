9706660981ba9399b55f8f0a6b0fbdaab8cf2a23
Eveshogweyore Alle <alleeveshogweyore@gmail.com>
2024-03-26 10:32:18 +0100

    Modify logic to handle memory allocation error.

--- 0-binary_tree_node.c
+++ 0-binary_tree_node.c
@@ -12,6 +12,11 @@ binary_tree_t *binary_tree_node(binary_tree_t *parent, int value)
 {
 	binary_tree_t *newNode = malloc(sizeof(binary_tree_t));
 
+	if (!newNode)
+	{
+		return (NULL);
+	}
+
 	newNode->n = value;
 	newNode->left = NULL;
 	newNode->right = NULL;
